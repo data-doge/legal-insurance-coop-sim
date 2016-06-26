e703227adf560aa24acce2a58e54fd5b48798af0
euglazer <eugenestapler@gmail.com>
2016-06-26 13:14:38 -0400

    can add settlement

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var $ = require('jquery')
+var toObject = require('form-to-object')
 
-var lawyerCount = 1, lawyerYearlyRate = 65000, clientCapacityPerLawyer = 30
+var lawyerCount = 1, lawyerYearlyRate = 65000, clientCapacityPerLawyer = 30, settlementContributions = 0
 
 update()
-$('input').on('change keydown keypress keyup', update)
+
+$('input').on('change keydown keypress keyup click', update)
+
+$('#add-settlement-form').on('submit', function (e) {
+  e.preventDefault()
+  var data = toObject(this)
+  settlementContributions += parseInt(data.percentContribution) / 100 * parseInt(data.amount)
+  update()
+})
 
 function update () {
   var lawyerCount = parseInt($('#lawyer-count').val())
@@ -12,12 +21,17 @@ function update () {
 
   $('#excess-capital').text(excessCapital(lawyerCount, numberOfMembers, monthlyPayment))
   $('#coverage-capacity').text(coverageCapacity(numberOfMembers, lawyerCount))
+  $('#settlement-contributions').text(settlementContributions)
 }
 
 function excessCapital (lawyerCount, numberOfMembers, monthlyPayment) {
-  return parseInt((numberOfMembers * monthlyPayment * 12) - (lawyerCount * lawyerYearlyRate))
+  return (numberOfMembers * monthlyPayment * 12) - (lawyerCount * lawyerYearlyRate) + settlementContributions
 }
 
 function coverageCapacity (numberOfMembers, lawyerCount) {
   return parseInt(lawyerCount * clientCapacityPerLawyer / numberOfMembers * 100)
+}
+
+function getSettlementContributions () {
+  return settlementContributions
 }
