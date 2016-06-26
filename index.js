20a2baa964b8612f1f278580532cd2da6bf99ddd
euglazer <eugenestapler@gmail.com>
2016-06-26 12:54:43 -0400

    show excess capital at end of year, and allow monthly payment to be variable

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 var $ = require('jquery')
 
-var lawyerCount = 1
-var lawyerYearlyRate = 65000
-var clientCapacityPerLawyer = 30
+var lawyerCount = 1, lawyerYearlyRate = 65000, clientCapacityPerLawyer = 30
 
-function monthlyPayment (numberOfMembers, lawyerCount) {
-  return parseInt(lawyerCount * lawyerYearlyRate / 12 / numberOfMembers)
+update()
+$('input').on('change keydown keypress keyup', update)
+
+function update () {
+  var lawyerCount = parseInt($('#lawyer-count').val())
+  var numberOfMembers = parseInt($('#number-of-members').val())
+  var monthlyPayment = parseInt($('#monthly-payment').val())
+
+  $('#excess-capital').text(excessCapital(lawyerCount, numberOfMembers, monthlyPayment))
+  $('#coverage-capacity').text(coverageCapacity(numberOfMembers, lawyerCount))
+}
+
+function excessCapital (lawyerCount, numberOfMembers, monthlyPayment) {
+  return parseInt((numberOfMembers * monthlyPayment * 12) - (lawyerCount * lawyerYearlyRate))
 }
 
 function coverageCapacity (numberOfMembers, lawyerCount) {
   return parseInt(lawyerCount * clientCapacityPerLawyer / numberOfMembers * 100)
-}
-
-update()
-
-$('input').on('change keydown keypress keyup', update)
-
-function update () {
- var lawyerCount = parseInt($('#lawyer-count').val())
- var numberOfMembers = parseInt($('#number-of-members').val())
- $('#monthly-payment').text(monthlyPayment(numberOfMembers, lawyerCount))
- $('#coverage-capacity').text(coverageCapacity(numberOfMembers, lawyerCount))
 }
